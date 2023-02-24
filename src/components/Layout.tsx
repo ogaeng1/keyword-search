1efd1f05e7171f96c7980c8984d2bcd623514b96
OH <rudals2148@naver.com>
2023-02-24 23:17:27 +0900

    [new] 초기 세팅

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/layout.scss";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children } : LayoutProps) => {
+    return (
+        <div>{children}</div>
+    )
+}
+
+export default Layout;
