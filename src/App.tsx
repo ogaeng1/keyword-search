8dbcdf073607ea73a5f04c8ee253ad566365c46b
OH <rudals2148@naver.com>
2023-02-28 18:50:08 +0900

    [fix] 다크모드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import Layout from "./components/Layout";
+import Search from "./components/Search";
+import ModeToggle from "./components/ModeToggle";
 
 function App() {
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "light");
+  }, [])
+  
   return (
     <Layout>
-      <div>ㅋㅋ</div>
+      <ModeToggle />
+      <Search />
     </Layout>
   );
 }
