6315b7d26b0bd9d8ec2097d2c01c7ce86d450cb7
OH <rudals2148@naver.com>
2023-02-28 21:10:14 +0900

    [fix] 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useEffect } from "react";
 import Layout from "./components/Layout";
 import Search from "./components/Search";
 import ModeToggle from "./components/ModeToggle";
 
 function App() {
-
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", "light");
-  }, [])
-  
   return (
     <Layout>
       <ModeToggle />
